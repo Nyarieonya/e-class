75eca3fe52977bf51b254fa7050f67c341eb22f3
Nyarieonya <132654289+Nyarieonya@users.noreply.github.com>
2023-05-22 10:24:04 +0900

    Add files via upload

--- homework01/screen.c
+++ homework01/screen.c
@@ -6,12 +6,12 @@
 #define Screen_Height 20
 
 char Screen_arr[Screen_Height * 100];
-const char* Title = "대학생활에서 살아남기";
+const char* Title = "대학교에서 살아남기";
 const char* Command_Text[] = { "1. G a m e  S t a r t", "2. G a m e  A b o u t", "3. G a m e  E x i t" };
 const char* Exit_Text[] = {"Y. Y E S     N. N O       ","게임을 종료하시겠습니까?"};
 const char* About_Text[] = { "만든사람 : 문수철-202327049", "Ver 0.1", "Q를 눌러 나가기" };
 const char* Story_Number = "주의사항 및 게임의 플레이 방법";
-const char* Story_Text1[] = {"이 게임은 매우 지극히 주관적이며 실제와 다를 수 있습니다. ","일부 스토리는 개발자의 실화를 바탕으로 만들어졌습니다.", "이 게임은 Enter키를 눌러 넘길 수도 있고 다른 입력을 요구 할 수 있습니다."};
+const char* Story_Text1[] = {"이 게임은 매우 지극히 주관적이며 실제와 다를 수 있습니다. ","스토리중 일부는 개발자의 실화를 바탕으로 만들어졌습니다.", "이 게임은 Enter키를 눌러 넘길 수도 있고 다른 입력을 요구 할 수 있습니다."};
 
 void Screen_Text_add(const char* source, int line, int col) { //스크린 변수에 source 텍스트를 추가하는 함수입니다.
     int len = strlen(source), st;
